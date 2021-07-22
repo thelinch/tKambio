6a8e7f926b056caf528189da0f8a72ee427d6c59
antony.inga@unas.edu.pe <antony.inga@unas.edu.pe>
2021-07-22 10:48:01 -0500

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,13 @@
 <html>
 
 <head>
-    <title>App Name - @yield('title')</title>
+    <title>TC</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
 </head>
 
 <body>
-  
+
 
     <div class="container">
         @yield('content')
--- resources/views/typeChange/list.blade.php
+++ resources/views/typeChange/list.blade.php
@@ -22,7 +22,7 @@
             <td>{{ $typeChange['tc_venta'] }}</td>
             <td>{{ $typeChange['tc_compra'] }}</td>
             <td>
-                <a class="btn btn-primary mr-2" href="/tc/edit/{{$typeChange['id']}}">Editar</a><a class="btn btn-danger" href="/tc/delete/{{$typeChange['id']}}">Eliminar</a>
+                <a class="btn btn-primary mr-2" href="/tc/edit/{{$typeChange['id']}}">Editar</a><a class="btn btn-danger" click="deleteTc()" data-id="{{$typeChange['id']}}">Eliminar</a>
             </td>
 
         </tr>
@@ -30,7 +30,45 @@
         @endforeach
     </tbody>
 </table>
-<ul>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
-</ul>
+<script>
+    (function() {
+        init()
+        // your page initialization code here
+        // the DOM will be available here
+
+    })();
+
+
+    async function deleteTc(tcId) {
+
+        console.log("eliminjneto", "/tc/delete/" + tcId)
+        const {
+            isConfirmed
+        } = await Swal.fire({
+            title: 'Desea Eliminar?',
+            text: 'La accion es irreversible,aun asi desea continuar?',
+            icon: 'question',
+            showCancelButton: true
+        })
+        if (isConfirmed) {
+            await fetch("/tc/delete/" + tcId)
+            window.location.reload()
+        }
+    }
+
+    function init() {
+        console.log("ejecutando...")
+        const deleteTcButtons = document.getElementsByClassName("btn-danger")
+        for (let index = 0; index < deleteTcButtons.length; index++) {
+            const element = deleteTcButtons[index];
+            element.addEventListener("click", function(event) {
+                // presentar la cuenta de clicks realizados sobre el elemento con id "prueba"
+                deleteTc(this.getAttribute('data-id'))
+            }, false);
+
+        }
+    }
+</script>
 @endsection
